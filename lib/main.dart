4eaa5b535ce64892aa266acd2e302fd1f2c224c4
Your Name <your.email@example.com>
2025-01-06 13:24:56 +0500

    auth part doen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:inventory_management_app/view/Screens/authentication/back_to_login.dart';
 import 'package:inventory_management_app/view/Screens/home_section/home_screen.dart';
 
 import 'view/Screens/authentication/forgot_password.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       designSize: Size(mediaQuerySize.width, mediaQuerySize.height),
       minTextAdapt: true,
       splitScreenMode: true,
-      child:  GetMaterialApp(debugShowCheckedModeBanner: false, home: OtpVerificationScreen()),
+      child:  GetMaterialApp(debugShowCheckedModeBanner: false, home: SignupScreen()),
     );
   }
 }
